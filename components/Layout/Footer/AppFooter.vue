88f4256b05c6b6e8f4fe1659826f2b97643ab8db
Herlambang Kuswicaksonojati <herlambangk@Herlambangs-MacBook-Air.local>
2024-10-07 11:04:15 +0700

    footer

--- components/Layout/Footer/AppFooter.vue
+++ components/Layout/Footer/AppFooter.vue
@@ -1,28 +1,25 @@
 <template>
   <footer class="mx-auto border-t border-gray-200 px-4 py-5 text-gray-500 dark:border-gray-800">
-    <div class="max-w-lg sm:mx-auto sm:text-center">
-      <Icon name="i-mdi-nuxt" class="h-10 w-10 text-purple-400" />
+    <div class="max-w-lg sm:mx-auto sm:text-center flex flex-col items-center">
+      <NuxtImg src="/taratech_logo.png" class="h-10  text-purple-400" />
       <p class="mt-2 text-[15px] leading-relaxed">
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-        unknown printer took a galley of type and scrambled it to make a type specimen book.
+        TaraTech didirikan pada Desember 2023 oleh tim yang memiliki rekam jejak kuat di startup teknologi dan berbagai
+        industri di Indonesia, mulai dari UMKM hingga industri besar. Kami memahami tantangan dan kebutuhan unik yang
+        dihadapi oleh bisnis di berbagai sektor, dari yang kecil hingga yang telah mapan.
+
       </p>
     </div>
     <ul
-      class="mt-8 items-center justify-center space-y-5 text-gray-700 dark:text-gray-300/90 sm:flex sm:space-x-4 sm:space-y-0"
-    >
-      <li
-        v-for="item in footerNavs"
-        :key="item.name"
-        class="hover:text-gray-500 dark:hover:text-gray-100"
-      >
-        <a href="#">
+      class="mt-8 items-center justify-center space-y-5 text-gray-700 dark:text-gray-300/90 sm:flex sm:space-x-4 sm:space-y-0">
+      <li v-for="item in footerNavs" :key="item.name" class="hover:text-gray-500 dark:hover:text-gray-100">
+        <a :href="item.href">
           {{ item.name }}
         </a>
       </li>
     </ul>
     <div class="mt-8 items-center justify-center sm:flex">
       <div class="mt-4 items-center text-center text-sm sm:mt-0">
-        Copyright &copy; {{ new Date().getFullYear() }}. All rights reserved.
+        Copyright &copy; {{ new Date().getFullYear() }} Taratech. All rights reserved.
       </div>
       <div class="mt-6 sm:mt-0">
         <ul class="flex items-center space-x-4">
@@ -38,27 +35,28 @@
 </template>
 
 <script setup lang="ts">
-  const footerNavs = [
-    {
-      href: "javascript:void()",
-      name: "About",
-    },
-    {
-      href: "javascript:void()",
-      name: "Blog",
-    },
-    {
-      href: "javascript:void()",
-      name: "Team",
-    },
-    {
-      href: "javascript:void()",
-      name: "Careers",
-    },
+const footerNavs = [
+  {
+    href: "#tentang",
+    name: "About",
+  },
 
-    {
-      href: "javascript:void()",
-      name: "Suport",
-    },
-  ];
+  {
+    href: "#portofolio",
+    name: "Portofolio",
+  },
+  {
+    href: "#services",
+    name: "Service",
+  },
+  {
+    href: "#client",
+    name: "client",
+  },
+
+  {
+    href: "#contact",
+    name: "kontak",
+  },
+];
 </script>
